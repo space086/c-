585859157f1718a7b0d247cb98b7c4c36338feab
space086 <space_086@naver.com>
2023-03-27 23:31:19 +0900

    c언어 sutdy: printf() & scanf()

---
+++ BeginPrj/testEx.c
@@ -0,0 +1,14 @@
+/* printf() 와 scanf() 연습 */
+/*  2023.03.27  */
+
+
+  #include  <stdio.h>
+
+  int main (void) {
+      printf("1 : Hello World");
+      printf("2 : Hello World\n");
+      printf("3 : Hello World C Programming \n");
+      printf("4 : Hello World \n C Programming \n");
+
+      return 0 ;
+  }
