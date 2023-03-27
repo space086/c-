36ddf117f99d419a6c94102c8008438e793d59ba
space086 <space_086@naver.com>
2023-03-27 23:41:54 +0900

    c언어 sutdy: printf() & scanf()

--- BeginPrj/testEx2.c
+++ BeginPrj/testEx2.c
@@ -6,5 +6,8 @@ int main(void) {
   // printf("%d", 1+2);
   printf("%d", 3/2);
   printf("\n\n\n");
+
+  printf("%8d \n", 12345); // 오른쪽 정렬
+  printf("%08d \n", 12345*1000); // 왼쪽 빈칸에 0을 넣고 싶다면
   return 0;
 }
