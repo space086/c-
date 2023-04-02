a0e60a2f8339e4dc5fb749b5806fe95437d15429
space086 <space_086@naver.com>
2023-04-02 22:14:37 +0900

    c언어 sutdy: define & const

---
+++ 0402-prj/ex-const.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(void) {
+  const int EX_RATE = 1120;
+  int dollor1 = 10;
+  int dollor2 = 100;
+  int dollor3 = 1000;
+
+  int won1 = EX_RATE * dollor1;
+  int won2 = EX_RATE * dollor2;
+  int won3 = EX_RATE * dollor3;
+
+
+  printf("%d원 = %d달러 \n", won1, dollor1);
+  printf("%d원 = %d달러 \n", won2, dollor2);
+  printf("%d원 = %d달러 \n", won3, dollor3);
+
+  return 0;
+}
