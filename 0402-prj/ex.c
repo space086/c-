7f253d5d61aae7db053ec80b1d30c0d0460dccd2
space086 <space_086@naver.com>
2023-04-02 22:05:56 +0900

    c언어 sutdy: 변수와 상수

---
+++ 0402-prj/ex.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  // 운영체제가 메인함수를 호출
+  // 변수 선언
+  // int    iData;
+  // double dData;
+  // char   ch;
+
+  // 변수에 값 할당
+  // iData = 50;
+  // dData = 7.7;
+  // ch = 'A';
+
+  // 변수 선언과 값 할당을 동시에
+  int iData = 50;
+  double dData = 7.7;
+  char ch = 'A';
+
+  // 출력
+  printf("iData = %d \n", iData); // 변수에 있는 값을 출력
+  printf("dData = %f \n", dData); // 변수에 있는 값을 출력
+  printf("ch = %c \n", ch); // 변수에 있는 값을 출력
+
+  return 0;
+}
